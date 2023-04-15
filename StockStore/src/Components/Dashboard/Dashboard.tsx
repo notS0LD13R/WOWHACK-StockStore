4ad1b41c4391366325672fdb488ff8c1d37b0a2a
notS0LD13R <joelsebin@gmail.com>
2023-04-16 05:19:02 +0530

    Added sidenav functionality

--- StockStore/src/Components/Dashboard/Dashboard.tsx
+++ StockStore/src/Components/Dashboard/Dashboard.tsx
@@ -4,13 +4,25 @@ import './Dashboard.scss'
 import SideNav from './SideNav/SideNav'
 import TopNav from './TopNav/TopNav'
 import HoriScroller from './HoriScroller/HoriScroller'
+import Buy from './Buy/Buy'
+import Sell from './Sell/Sell'
+import Apply from './Apply/Apply'
+
+import { useState } from 'react'
 
 export default function Dashboard() {
+  const links:{[key:string]:JSX.Element}={
+    Top:<HoriScroller/>,
+    Buy: <Buy />,
+    Sell: <Sell />,
+    Apply: <Apply />
+  }
+  const [link,setLink]=useState('Top')
   return (
     <div className="Dashboard">
-      <SideNav/>
+      <SideNav setLink={setLink}/>
       <TopNav />
-      <HoriScroller/>
+      {links[link]}
     </div>
   )
 }
