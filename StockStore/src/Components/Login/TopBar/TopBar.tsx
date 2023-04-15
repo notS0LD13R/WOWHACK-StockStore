5db3b8f121527921509de273f472770b0b444004
Alnas Kabeer <alnaskabeer@gmail.com>
2023-04-15 14:11:31 +0530

    topbar done

--- StockStore/src/Components/Login/TopBar/TopBar.tsx
+++ StockStore/src/Components/Login/TopBar/TopBar.tsx
@@ -1,18 +1,34 @@
 import "./TopBar.scss";
+import logo from "../../../assets/logo.png";
 
 export default function TopBar() {
   return (
     <div className="TopBar">
       {/* <div className="background" /> */}
       <div className="icon">
-        <img src="../../../assets/Logo.png" alt="icon" />
+        <img src={logo} alt="icon" />
       </div>
       <div className="actionButtons">
         {/* Other buttons */}
-        <div>Button 1</div>
-        <div>Button 2</div>
-        <div>Button 3</div>
+        <ActionButton text="About us" />
+        <ActionButton text="Features" />
+        <ActionButton text="Traders" />
+        <ActionButton text="Featured Stocks" />
+        <ActionButton text="Contact Us" />
+        <ActionButton text="Login/Signup" bold={true} />
       </div>
+    </div>
+  );
+}
+
+function ActionButton(props: {
+  text: string;
+  onClick?: () => void;
+  bold?: boolean;
+}) {
+  return (
+    <div className={"actionButton" + (props.bold == true ? " bold" : "")}>
+      {props.text}
     </div>
   );
 }
