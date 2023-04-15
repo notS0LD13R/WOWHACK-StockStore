a5081f766f693c9a381ff96ecfd422fb47d530c4
Alnas Kabeer <alnaskabeer@gmail.com>
2023-04-16 05:19:08 +0530

    hehe

---
+++ StockStore/src/Components/BuyStocks/BuyStocks.tsx
@@ -0,0 +1,12 @@
+export default function BuyStocks() {
+  return <div className="BuyStocks"></div>;
+}
+
+function BuyStockPills() {
+  return (
+    <div className="BuyStockPills">
+      <div>info</div>
+      <div>Buy</div>
+    </div>
+  );
+}
--- StockStore/src/Components/Dashboard/SideNav/SideNav.tsx
+++ StockStore/src/Components/Dashboard/SideNav/SideNav.tsx
@@ -1,55 +1,52 @@
-import React, { useState } from 'react'
-import './SideNav.scss'
+import React, { useState } from "react";
+import "./SideNav.scss";
 
-import {MdInventory,MdShoppingCart,MdHelp} from 'react-icons/md'
-import {FaFileInvoiceDollar,FaScroll} from 'react-icons/fa'
-import {AiFillSetting,AiOutlineMenu} from 'react-icons/ai'
+import { MdInventory, MdShoppingCart, MdHelp } from "react-icons/md";
+import { FaFileInvoiceDollar, FaScroll } from "react-icons/fa";
+import { AiFillSetting, AiOutlineMenu } from "react-icons/ai";
 
 export default function SideNav() {
-    const iconAttr={
-        color:'E1CC5B',
-        size:'2.5rem'
-    }
+  const iconAttr = {
+    color: "E1CC5B",
+    size: "2.5rem",
+  };
 
-    const [open,setOpen]=useState(false)
+  const [open, setOpen] = useState(false);
 
-    const handleMenuClick=()=>{
-        setOpen(!open)
-    }
+  const handleMenuClick = () => {
+    setOpen(!open);
+  };
 
-  
-    return (
-    <div className={`SideNav ${+open?'open':''}`}>
-        <span>
-            <AiOutlineMenu color='white' size='2.5rem' onClick={handleMenuClick}/>
-        </span>
+  return (
+    <div className={`SideNav ${+open ? "open" : ""}`}>
+      <span>
+        <AiOutlineMenu color="white" size="2.5rem" onClick={handleMenuClick} />
+      </span>
 
-        <span>
-            <MdInventory {...iconAttr}/>
-            <i>Stocks of the Day</i>
-        </span>
-        <span>
-            <MdShoppingCart {...iconAttr}/>
-            <i>Buy Stocks</i>
-        </span>
-        <span>
-            <FaFileInvoiceDollar {...iconAttr}/>
-            <i>Sell Stocks</i>
-        </span>
-        <span>
-            <FaScroll {...iconAttr} />
-            <i>Apply for Stock</i>
-
-        </span>
-        <span>
-            <AiFillSetting {...iconAttr} />
-            <i>Setting</i>
-
-        </span>
-        <span>
-            <MdHelp {...iconAttr} />
-            <i>Help</i>
-        </span>
+      <span>
+        <MdInventory {...iconAttr} />
+        <i>Stocks of the Day</i>
+      </span>
+      <span>
+        <MdShoppingCart {...iconAttr} />
+        <i>Buy Stocks</i>
+      </span>
+      <span>
+        <FaFileInvoiceDollar {...iconAttr} />
+        <i>Sell Stocks</i>
+      </span>
+      <span>
+        <FaScroll {...iconAttr} />
+        <i>Apply for Stock</i>
+      </span>
+      <span>
+        <AiFillSetting {...iconAttr} />
+        <i>Setting</i>
+      </span>
+      <span>
+        <MdHelp {...iconAttr} />
+        <i>Help</i>
+      </span>
     </div>
-  )
+  );
 }
